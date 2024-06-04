c1e66b5f7b83e8bb84be9ca169380561bab61c4d
Fabio A. González <fagonzalezo@users.noreply.github.com>
2024-06-03 19:13:27 -0500

    Update  kdm_classification.ipynb, add kdm_density_estimation.ipynb

--- kdm/models/mem_kdm_class_model_wrapper.py
+++ kdm/models/mem_kdm_class_model_wrapper.py
@@ -14,6 +14,7 @@ class MemKDMClassModelWrapper:
                  samples_y,
                  encoder,
                  n_comp,
+                 index_type='Flat',
                  sigma=0.1,
                  **kargs):
         self.dim_y = dim_y
@@ -23,9 +24,6 @@ class MemKDMClassModelWrapper:
         self.samples_y = samples_y
         samples_x = torch.tensor(samples_x, dtype=torch.float32, device='cpu')
         dataset = torch.utils.data.TensorDataset(samples_x)
-        nlist = 100
-        self.index = faiss.IndexFlatL2(encoded_size)  
-        #self.index = faiss.IndexHNSWFlat(encoded_size, 32)
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=32)
         self.samples_x_enc = np.zeros((samples_x.shape[0], encoded_size))
         i = 0
@@ -33,9 +31,13 @@ class MemKDMClassModelWrapper:
             x_batch = x_batch[0]
             enc_batch = self.encoder(x_batch)
             enc_batch = keras.ops.convert_to_numpy(enc_batch)
-            self.index.add(enc_batch)
             self.samples_x_enc[i:i+enc_batch.shape[0]] = enc_batch
             i += enc_batch.shape[0]
+        self.index = faiss.index_factory(encoded_size, index_type, faiss.METRIC_L2)
+        self.index.train(self.samples_x_enc)
+        self.index.add(self.samples_x_enc)
+        #self.index = faiss.IndexFlatL2(encoded_size)  
+        #self.index = faiss.IndexHNSWFlat(encoded_size, 32)
         self.model = MemKDMClassModel(
                  encoded_size,
                  dim_y,
@@ -80,25 +82,50 @@ class MemKDMClassModelWrapper:
     def compile(self, optimizer, loss, metrics=None):
         self.model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
-    def fit(self, batch_size=32, epochs=1, verbose=1):
-        dataset = CustomDataset(self.samples_x_enc, 
-                                self.samples_y, 
-                                self.index, 
-                                self.n_comp,
-                                batch_size=batch_size)
-        return self.model.fit(dataset, epochs=epochs, verbose=verbose)
+    def fit(self, X=None, y=None, batch_size=32, epochs=1, verbose=1, callbacks=None):
+        '''
+        Fit the model using the given data. If X is None, it will use the samples
+        used to build the index. Otherwise it will use the given data. 
+        '''
+        if X is None:
+            dataset = TrainDataset(batch_size=batch_size,
+                                   mkdm_model=self,
+                                   use_index_samples=True)
+            return self.model.fit(dataset, epochs=epochs, verbose=verbose, callbacks=callbacks)
+        else:
+            assert y is not None and len(X) == len(y), 'X and y must have the same length'
+            dataset = TrainDataset(batch_size=batch_size,
+                                   mkdm_model=self,
+                                   samples_x=X,
+                                   samples_y=y, 
+                                   use_index_samples=False)
+            return self.model.fit(dataset, epochs=epochs, verbose=verbose, callbacks=callbacks)
+        
+    def load_weights(self, filepath):
+        self.model.load_weights(filepath)
 
-class CustomDataset(keras.utils.PyDataset):
-    def __init__(self, samples_x_enc, 
-                 samples_y, index, 
-                 n_comp, batch_size, 
+    def save_weights(self, filepath):
+        self.model.save_weights(filepath)
+            
+class TrainDataset(keras.utils.PyDataset):
+    def __init__(self,  
+                 batch_size,
+                 mkdm_model,
+                 samples_x=None, 
+                 samples_y=None,
+                 use_index_samples=True,
                  **kwargs):
         super().__init__(**kwargs)
-        self.samples_x_enc = samples_x_enc
-        self.samples_y = samples_y
-        self.index = index
-        self.n_comp = n_comp
+        if use_index_samples:
+            self.samples_y = mkdm_model.samples_y
+        else:
+            assert samples_x is not None and samples_y is not None, 'samples_x and samples_y must be provided'
+            self.samples_x = samples_x
+            self.samples_y = samples_y            
         self.batch_size = batch_size
+        self.mkdm_model = mkdm_model
+        self.use_index_samples = use_index_samples
+
 
     def __len__(self):
         return math.ceil(len(self.samples_y) / self.batch_size)
@@ -108,8 +135,14 @@ class CustomDataset(keras.utils.PyDataset):
         # Cap upper bound at array length; the last batch may be smaller
         # if the total number of items is not a multiple of batch size.
         high = min(low + self.batch_size, len(self.samples_y))
-        x_enc = self.samples_x_enc[low:high]
-        _, I = self.index.search(x_enc, self.n_comp + 1)
-        x_neigh = np.take(self.samples_x_enc, I, axis=0)
-        y_neigh = np.take(self.samples_y, I, axis=0)
-        return (x_enc, x_neigh[:, 1:], y_neigh[:,1:]), self.samples_y[low:high]
+        if self.use_index_samples:
+            x_enc = self.mkdm_model.samples_x_enc[low:high]
+            n_comp = self.mkdm_model.n_comp + 1
+        else:
+            x_enc = self.mkdm_model.encoder(self.samples_x[low:high])
+            n_comp = self.mkdm_model.n_comp
+        _, I = self.mkdm_model.index.search(x_enc, n_comp)
+        x_neigh = np.take(self.mkdm_model.samples_x_enc, I, axis=0)
+        y_neigh = np.take(self.mkdm_model.samples_y, I, axis=0)
+        n_comp = self.mkdm_model.n_comp
+        return (x_enc, x_neigh[:, -n_comp:], y_neigh[:, -n_comp:]), self.samples_y[low:high]
